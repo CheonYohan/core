c50525e8cbebf1127f43d1e7877773d6f1c1e5db
cheonYohan <kero74@naver.com>
2022-03-07 22:25:55 +0900

    Spring ApplicationContextBean Test

--- spring/src/main/java/hello/core/order/RateDiscountPolicy.java
+++ spring/src/main/java/hello/core/order/RateDiscountPolicy.java
@@ -1,12 +1,17 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 
 @Component
+@MainDiscountPolicy
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
