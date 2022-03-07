23651d6f16f9716c5aaa058bbad8c93e67daa180
cheonYohan <kero74@naver.com>
2022-03-07 21:41:15 +0900

    Spring ApplicationContextBean Test

--- spring/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ spring/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository {
 
     // 일반 해쉬맵을 사용하면 동시성 문제가 발생 할 수 있다. 실무에서 적용하려면 ConcurrentHashMap 을 사용해야한다. ConcurrentHashMap은 따로 공부해보자.
